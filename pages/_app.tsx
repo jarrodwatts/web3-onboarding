1c5fb804ee9187f13c7a832cec05becf6544f540
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2023-05-08 16:44:02 +1000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         smartWallet({
           factoryAddress: "0x69608a6fE2e1Ce34eAcC4688502b2a2A1209EE2c", // Address of your account factory smart contract
           thirdwebApiKey:
-            "e8472076e04067de52bd9b2251296f60d1da22f07e4c60c6cf8bbcfb3590aa474d045c0c62aeb2f7372b497f30def195f2152625a3603760352461dcbd0e0ed3", // The API key you got from the previous step
+            "e8472076e04067de52bd9b2251296f60d1da22f07e4c60c6cf8bbcfb3590aa474d045c0c62aeb2f7372b497f30def195f2152625a3603760352461dcbd0e0ed3",  // Your thirdweb API key
           gasless: true,
           personalWallets: [
             localWallet({
